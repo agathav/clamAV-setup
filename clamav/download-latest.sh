7d769c76fde068ce1480ce4f08b5c5f59179188b
pantzan <panagiotist@mail.com>
2019-03-16 21:16:59 +0100

    make sure that all commands are successful before exiting, silent wget to show only progress bar

--- clamav/download-latest.sh
+++ clamav/download-latest.sh
@@ -8,21 +8,27 @@
 #
 
 # Download and extract to clamav-latest ----------------------------------------
+STATUS="F"
 
-wget -r --no-parent -A 'clamav-*.tar.gz' -P . https://www.clamav.net/downloads/
-mv www.clamav.net/downloads/production/clamav-*.tar.gz ./clamav-latest.tar.gz
-tar -xzf clamav-latest.tar.gz
-mkdir -p clamav-latest && tar -xzf clamav-latest.tar.gz -C clamav-latest --strip-components 1
-rm -rf clamav-latest.tar.gz www.clamav.net
+wget -r --no-parent -A 'clamav-*.tar.gz' -q --show-progress -P . https://www.clamav.net/downloads/  &&
+mv www.clamav.net/downloads/production/clamav-*.tar.gz ./clamav-latest.tar.gz &&
+tar -xzf clamav-latest.tar.gz &&
+mkdir -p clamav-latest && tar -xzf clamav-latest.tar.gz -C clamav-latest --strip-components 1 &&
+rm -rf clamav-latest.tar.gz www.clamav.net &&
 
 # Delete all clamav-* directories except for clamav-latest ---------------------
 
-find . -type 'd' | grep "clamav-" | grep -v "clamav-latest" | xargs sudo rm -rf
+find . -type 'd' | grep "clamav-" | grep -v "clamav-latest" | xargs sudo rm -rf && STATUS="S"
 
 # Print info and exit ----------------------------------------------------------
+echo
+if [ "$STATUS" == "S" ];
+then
+  echo "Download complete"
+  echo "Enter clamav-latest to install the latest version of ClamAV"
+  exit 0
+else
+  echo "The installation failed. Exiting"
+  exit 1
 
-echo
-echo "Download complete"
-echo "Enter clamav-latest to install the latest version of ClamAV"
-echo
-exit 0
+fi
